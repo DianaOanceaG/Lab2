f594b5fd6707b7cba50fb88bfcef7d744119c151
DianaOanceaG <diana_oancea91@yahoo.com>
2014-10-26 16:15:54 +0200

    Add Lab2

---
+++ Lab2/src/com/example/lab2/Main.java
@@ -0,0 +1,10 @@
+package com.example.lab2;
+
+/**
+ * Created by Diana on 25.10.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Lab2");
+    }
+}
